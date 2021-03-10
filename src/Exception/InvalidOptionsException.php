6e412c35af82c7bbfca317a9be61e8addb12563f
gggeek <giunta.gaetano@gmail.com>
2021-03-10 23:10:35 +0000

    fix throwing exception on invalid constraint def

--- src/Exception/InvalidOptionsException.php
+++ src/Exception/InvalidOptionsException.php
@@ -2,6 +2,21 @@
 
 namespace TanoConsulting\DataValidatorBundle\Exception;
 
+use Throwable;
+
 class InvalidOptionsException extends ValidatorException
 {
+    protected $options;
+
+    public function __construct(string $message, array $options, Throwable $previous = NULL)
+    {
+        parent::__construct($message, 0, $previous);
+
+        $this->options = $options;
+    }
+
+    public function getOptions()
+    {
+        return $this->options;
+    }
 }
